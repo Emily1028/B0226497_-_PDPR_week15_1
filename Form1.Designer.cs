ed249dde7d1077bd4b30ee03c2b51e43d46af775
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-02 00:01:44 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace B0226497_段昱如_PDPR_week15_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEXIT = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.lblRESULT = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "B0226497_段昱如_PDPR_week15_1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "類別：";
+            // 
+            // btnEXIT
+            // 
+            this.btnEXIT.Location = new System.Drawing.Point(175, 387);
+            this.btnEXIT.Name = "btnEXIT";
+            this.btnEXIT.Size = new System.Drawing.Size(122, 37);
+            this.btnEXIT.TabIndex = 2;
+            this.btnEXIT.Text = "離開";
+            this.btnEXIT.UseVisualStyleBackColor = true;
+            this.btnEXIT.Click += new System.EventHandler(this.btnEXIT_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(24, 387);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(122, 37);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "確定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "愛情",
+            "懸疑",
+            "動畫"});
+            this.comboBox1.Location = new System.Drawing.Point(108, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 31);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(24, 107);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(706, 274);
+            this.checkedListBox1.TabIndex = 5;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // lblRESULT
+            // 
+            this.lblRESULT.AutoSize = true;
+            this.lblRESULT.Location = new System.Drawing.Point(3, 9);
+            this.lblRESULT.Name = "lblRESULT";
+            this.lblRESULT.Size = new System.Drawing.Size(82, 24);
+            this.lblRESULT.TabIndex = 6;
+            this.lblRESULT.Text = "結果：";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.lblRESULT);
+            this.panel1.Location = new System.Drawing.Point(24, 430);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(706, 130);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 572);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnEXIT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "B0226497_段昱如_PDPR_week15_1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEXIT;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label lblRESULT;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B0226497_段昱如_PDPR_week15_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string str = "";
+            for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                str += $"{checkedListBox1.CheckedItems[i]} ";
+            }
+            lblRESULT.Text += $"\n選取{comboBox1.Text}類別：{str}";
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    List<string> romantic = new List<string>();
+                    checkedListBox1.Items.Clear();
+                    romantic.Add("樂來越愛你");
+                    checkedListBox1.Items.Add(romantic[0]);
+                    romantic.Add("手札情緣");
+                    checkedListBox1.Items.Add(romantic[1]);
+                    romantic.Add("婚姻故事");
+                    checkedListBox1.Items.Add(romantic[2]);
+                    romantic.Add("真愛挑日子");
+                    checkedListBox1.Items.Add(romantic[3]);
+                    romantic.Add("王牌冤家");
+                    checkedListBox1.Items.Add(romantic[4]);
+                    break;
+                case 1:
+                    List<string> suspense = new List<string>();
+                    checkedListBox1.Items.Clear();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        suspense.Add("佈局");
+                        suspense.Add("幻象");
+                        suspense.Add("神秘沼澤");
+                        suspense.Add("屍物招領");
+                        suspense.Add("凌刑密密縫");
+                        checkedListBox1.Items.Add(suspense[i]);
+                    }
+                    break;
+                case 2:
+                    List<string> animated = new List<string>();
+                    checkedListBox1.Items.Clear();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        animated.Add("風起");
+                        animated.Add("龍貓");
+                        animated.Add("神隱少女");
+                        animated.Add("龍與雀斑公主");
+                        animated.Add("想哭的我戴上貓的面具");
+                        checkedListBox1.Items.Add(animated[i]);
+                    }
+                    break;
+            }
+        }
+
+        private void btnEXIT_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
